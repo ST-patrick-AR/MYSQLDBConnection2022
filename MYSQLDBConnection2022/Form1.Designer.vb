6bcf6696ca7d8c92df53e7530644d8f38dffe5df
REF2024 <REF2024@PL34>
2024-11-12 11:55:51 +0800

    Add project files.

---
+++ MYSQLDBConnection2022/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(543, 264)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(226, 61)
+        Button1.TabIndex = 0
+        Button1.Text = "MYSQL TEST"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 22F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1257, 660)
+        Controls.Add(Button1)
+        Font = New Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(5, 4, 5, 4)
+        Name = "Form1"
+        Text = "MYSQL DATABASE CONNECTION"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+
+End Class
